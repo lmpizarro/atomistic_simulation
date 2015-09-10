aafc7f6c42dd995a66a313bc6435abe6a2ad9d76
Pablo Bellino <pbellino@gmail.com>
2015-09-10 08:08:18 -0300

    Separo a una carpeta con la simulación hecha en MATLAB de ruido neutrónico

--- private/is_octave.m
+++
@@ -1,9 +0,0 @@
- function r = is_octave ()
-%%  subfunction that checks if we are in octave
-%  Usada para corregir incompatibilidades entre octave y matlab
-  persistent x;
-   if (isempty (x))
-     x = exist ('OCTAVE_VERSION', 'builtin');
-   end
-   r = x;
- end
