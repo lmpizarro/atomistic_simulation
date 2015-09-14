4c88ba3752e9402503f6cab182f5b9a755875805
Pablo Bellino <pbellino@gmail.com>
2015-09-14 13:46:33 -0300

    Se escribe el script de python para correr los trabajos

--- FORTRAN/ising/corridas.py
+++ FORTRAN/ising/corridas.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+import os
+import shutil
+from subprocess import call
 
-tempe = [0.5, 0.6, 0.7, 1.2, 1.4]
+curr_dir= os.getcwd()
+
+tempe = [0.5, 0.6]
 tempe = [str(i) for i in tempe]
 
 for T in tempe:
@@ -13,6 +18,22 @@ for T in tempe:
     with open('parametros.dat','w') as fileout:
         fileout.write(new)
 
+    carpeta = T + '_tmpfolder'
+
+    path_carpeta = os.path.join(curr_dir,carpeta)
+    
+    try: 
+        os.makedirs(path_carpeta)
+    except OSError:
+#        print('Ya existe el directorio. Se omite corrida con T=' + T)
+        if not os.path.isdir(path_carpeta):
+            raise
+        
+    shutil.copy('parametros.dat',path_carpeta) 
+    os.chdir(path_carpeta)       
+    call(curr_dir+'/ising')
+    
+    os.chdir(curr_dir)
     # Creo directorio
     # copio ejecutable y entradas
     # Hago algo con la salida
