4747734b97b2384464e715256f7182e8c58a6c16
Pablo Bellino <pbellino@gmail.com>
2015-09-21 10:18:16 -0300

    Escribe tablas en notación científica. Se guardan también el porcentaje de rechazados con su correspondiente desviación estandar

--- FORTRAN/ising/corridas.py
+++ FORTRAN/ising/corridas.py
@@ -35,7 +35,7 @@ def escribe_semilla():
         fileout.write(str(entero_aleatorio))
         
 def copia_val_medios(T,N):
-    E=[];M=[];c=[];s=[]
+    E=[];M=[];c=[];s=[];a=[]
     # Copio los archivos de la carpeta local y los escribo en un archivo común
     with open('runs_estadistica.dat','r') as valruns:
         for line in valruns:
@@ -43,28 +43,37 @@ def copia_val_medios(T,N):
             E.append(col[2])
             c.append(col[3])            
             M.append(col[4])
-            s.append(col[5])      
+            s.append(col[5])
+            a.append(col[6])
     arch_comun = os.path.join(curr_dir,'tablas_temperatura.dat')
     fa = 1/np.sqrt(N)
-    valor = [T, np.mean(E), np.std(E)*fa, np.mean(M), np.std(M)*fa,
-                np.mean(c), np.std(c)*fa, np.mean(s), np.std(s)*fa]
+    valor = [float(T), np.mean(E), np.std(E)*fa, np.mean(M), np.std(M)*fa,
+                np.mean(c), np.std(c)*fa, np.mean(s), np.std(s)*fa,
+                np.mean(a), np.std(a)]
     # Abro el archivo donde se volcarán las resultados de cada temperatura   
-    with open(arch_comun,'a') as comun:
-        comun.write('  '.join(str(j) for j in valor)+'\n')        
+    with open(arch_comun,'a') as comun:  
+        valor_str = '  '.join( ('{:.5E}').format(j) for j in valor) 
+        comun.write( valor_str +'\n')   
             
 def copia_val_medios_runs(i):
     # Copio los archivos de la carpeta local y los escribo en un archivo común
     with open('val_medios.dat','r') as vmed:
         valor=vmed.readline()
+    # Lee el archivo con la cantidad de aceptaciones
+    with open('aceptaciones.dat','r') as archacept:
+        acep=archacept.readline()
+        colu = [float(x) for x in acep.split()]
+        # Guardará el porcentaje de aceptaciones
+        por_acep = 100.0*colu[1]/colu[2]
     arch_comun = os.path.join(path_carpeta,'runs_estadistica.dat')
     with open(arch_comun,'a') as comun:
-        comun.write(str(i)+' ' + valor)   
+        comun.write(str(i)+' ' + valor.rstrip() + ' ' + str(por_acep) + '\n')   
 
 # Directorio raíz donde está el ejecutable y este script
 curr_dir = os.getcwd()
 # Lista con las temperaturas que se desean calcular
 T_min = 0.5
-T_max = 3.0
+T_max = 1.0
 dT    = 0.1
 tempe = np.arange(T_min,T_max+dT,dT)
 #tempe = tempe.tolist() + [2.15, 2.25, 2.35, 2.45, 2.55] # Mäs detalle en la Tc
@@ -98,7 +107,7 @@ for T in tempe:
     # Cambia el archivo de entrada adentro de la carpeta
     escribe_entrada('T',T)
     # Corre el programa para ver la convergencia
-    escribe_entrada('N','40000') 
+    escribe_entrada('N','4000') 
 
    # Loop para correr N veces con los mismos parámetros para calcular el error
     for i in range(0,Nrun):
@@ -124,7 +133,7 @@ for T in tempe:
         os.rename('magneti.dat','magneti_terma.dat')    
     
         # Corre por segunda vez tomando el estado anterior. Aumento el N
-        escribe_entrada('N','1000000')
+        escribe_entrada('N','10000')
         print('Corriendo {} a la temperatura {}'.format(carpeta_runs,T))
         salida = subprocess.check_output(curr_dir+'/ising')
     
