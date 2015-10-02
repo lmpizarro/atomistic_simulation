b47ca374563ba28c84a7d657847463b67fdb8385
Pablo Bellino <pbellino@gmail.com>
2015-10-02 14:44:29 -0300

    Se agrega script grafico_tablas_tamano.py para graficar las diferencias observadas con distintos tamaños de la matriz de spines. Es un script especial de grafico_tablas.py

---
+++ FORTRAN/ising/src/grafico_tablas_tamano.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/python
+
+# Grafica los resultados para distintos tamaños de la red de spines
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) < 2:
+    archivo = ['tablas_temperatura.dat']
+else:
+    archivo = sys.argv[1:]
+    
+Nsp = []
+Nsp.append(10*10)
+Nsp.append(20*20)
+Nsp.append(30*30)
+Nsp.append(40*40)
+
+forma = []
+forma.append('k.')
+forma.append('b.')
+forma.append('g.')
+forma.append('m.')
+
+leg = []
+leg.append('10 x 10')
+leg.append('20 x 20')
+leg.append('30 x 30')
+leg.append('40 x 40')
+
+j = 0
+for nombre in archivo:
+
+    data = np.loadtxt(nombre)
+    
+    T = data[:,0]
+    E_avg = data[:,1]
+    E_std = data[:,2]
+    M_avg = np.abs(data[:,3])
+    M_std = data[:,4]
+    c_avg = data[:,5]
+    c_std = data[:,6]
+    s_avg = data[:,7]
+    s_std = data[:,8]
+    a_avg = data[:,9]
+    a_std = data[:,10]
+    
+    plt.figure(1)
+    
+    plt.subplot(2, 1, 1)
+    plt.errorbar(T, E_avg/Nsp[j], yerr=E_std/Nsp[j], fmt=forma[j],label=leg[j])
+    plt.title('Valores medios')
+    plt.xlabel('Temperatura')
+    plt.ylabel(u'Energía')
+    plt.ylim([-2.1 , -0.2])
+    plt.legend(loc='upper left',numpoints=1)
+   
+    
+    plt.subplot(2, 1, 2)
+    plt.errorbar(T, M_avg/Nsp[j],yerr=M_std/Nsp[j], fmt= forma[j],label=leg[j])
+    plt.xlabel('Temperatura')
+    plt.ylabel(u'Magnetización')
+    plt.ylim([-0.1 , 1.1])
+    plt.legend(loc='upper right',numpoints=1)    
+    
+    plt.figure(2)
+    
+    plt.subplot(2, 1, 1)
+    plt.errorbar(T, c_avg/Nsp[j], yerr=c_std/Nsp[j], fmt=forma[j],label=leg[j])
+    plt.title('Fluctuaciones')
+    plt.xlabel('Temperatura')
+    plt.ylabel(u'Calor específico')
+    plt.ylim([-0.1 , 2.3])
+    plt.legend(loc='upper right',numpoints=1)  
+    
+    plt.subplot(2, 1, 2)
+    plt.errorbar(T, s_avg/Nsp[j], yerr=s_std/Nsp[j], fmt=forma[j],label=leg[j])
+    plt.xlabel('Temperatura')
+    plt.ylabel(u'Suceptibilidad magnética')
+    plt.ylim([-4 , 120])
+    plt.legend(loc='upper right',numpoints=1)  
+    
+    plt.figure(3)
+    
+    plt.errorbar(T,a_avg,yerr=a_std,fmt=forma[j],label=leg[j])
+    plt.xlabel('Temperatura')
+    plt.ylabel('Aceptaciones [%]')
+    plt.ylim([-5 , 75])
+    plt.legend(loc='upper left',numpoints=1)
+    
+    j += 1
+
+plt.show()
