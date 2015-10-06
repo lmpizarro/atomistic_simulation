3c63debc0cff0bb727a49c76ebca6989cc97cedf
Pablo Bellino <pbellino@gmail.com>
2015-10-06 09:54:43 -0300

    Mejoras para graficar

--- FORTRAN/ising/src/grafico_tablas.py
+++ FORTRAN/ising/src/grafico_tablas.py
@@ -37,48 +37,53 @@ for nombre in archivo:
     a_avg = data[:,9]
     a_std = data[:,10]
     
-    plt.figure(1)
+    plt.figure(1,figsize=(8,8))
     
     plt.subplot(2, 1, 1)
     plt.errorbar(T, E_avg/Nsp[j], yerr=E_std/Nsp[j], fmt=forma[j],label=leg[j])
-    plt.title('Valores medios')
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Energía')
+    plt.title(r'Valores medios')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<E/J>/N$')
     plt.ylim([-2.1 , -0.2])
     plt.legend(loc='upper left',numpoints=1)
+    plt.grid(True)
    
     
     plt.subplot(2, 1, 2)
     plt.errorbar(T, M_avg/Nsp[j],yerr=M_std/Nsp[j], fmt= forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Magnetización')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<M/\mu>/N$')
     plt.ylim([-0.1 , 1.1])
     plt.legend(loc='upper right',numpoints=1)    
+    plt.grid(True)
     
-    plt.figure(2)
+    plt.figure(2,figsize=(8,8))
     
     plt.subplot(2, 1, 1)
     plt.errorbar(T, c_avg/Nsp[j], yerr=c_std/Nsp[j], fmt=forma[j],label=leg[j])
     plt.title('Fluctuaciones')
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Calor específico')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<C>/N$')
     plt.ylim([-0.1 , 2.3])
     plt.legend(loc='upper right',numpoints=1)  
+    plt.grid(True)
     
     plt.subplot(2, 1, 2)
     plt.errorbar(T, s_avg/Nsp[j], yerr=s_std/Nsp[j], fmt=forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Suceptibilidad magnética')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<\chi>/N$')
     plt.ylim([-4 , 100])
     plt.legend(loc='upper right',numpoints=1)  
+    plt.grid(True)
     
     plt.figure(3)
     
     plt.errorbar(T,a_avg,yerr=a_std,fmt=forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel('Aceptaciones [%]')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$Aceptaciones [\%]$')
     plt.ylim([-5 , 75])
     plt.legend(loc='upper left',numpoints=1)
+    plt.grid(True)
     
     j += 1
 
--- FORTRAN/ising/src/grafico_tablas_tamano.py
+++ FORTRAN/ising/src/grafico_tablas_tamano.py
@@ -16,20 +16,25 @@ else:
 Nsp = []
 Nsp.append(10*10)
 Nsp.append(20*20)
-Nsp.append(30*30)
+#Nsp.append(30*30)
 Nsp.append(40*40)
+#Nsp.append(90*90)
+Nsp.append(100*100)
 
 forma = []
 forma.append('k.')
 forma.append('b.')
 forma.append('g.')
+forma.append('r.')
 forma.append('m.')
 
 leg = []
 leg.append('10 x 10')
 leg.append('20 x 20')
-leg.append('30 x 30')
+#leg.append('30 x 30')
 leg.append('40 x 40')
+#leg.append('90 x 90')
+leg.append('100 x 100')
 
 j = 0
 for nombre in archivo:
@@ -48,48 +53,52 @@ for nombre in archivo:
     a_avg = data[:,9]
     a_std = data[:,10]
     
-    plt.figure(1)
+    plt.figure(1,figsize=(8,8))
     
     plt.subplot(2, 1, 1)
     plt.errorbar(T, E_avg/Nsp[j], yerr=E_std/Nsp[j], fmt=forma[j],label=leg[j])
     plt.title('Valores medios')
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Energía')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<E/J>/N$')
     plt.ylim([-2.1 , -0.2])
     plt.legend(loc='upper left',numpoints=1)
-   
+    plt.grid(True)
     
     plt.subplot(2, 1, 2)
     plt.errorbar(T, M_avg/Nsp[j],yerr=M_std/Nsp[j], fmt= forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Magnetización')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<M/\mu>/N$')
     plt.ylim([-0.1 , 1.1])
     plt.legend(loc='upper right',numpoints=1)    
+    plt.grid(True)
     
-    plt.figure(2)
+    plt.figure(2,figsize=(8,8))
     
     plt.subplot(2, 1, 1)
     plt.errorbar(T, c_avg/Nsp[j], yerr=c_std/Nsp[j], fmt=forma[j],label=leg[j])
     plt.title('Fluctuaciones')
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Calor específico')
-    plt.ylim([-0.1 , 2.3])
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<C>/N$')
+    plt.ylim([-0.1 , 2.5])
     plt.legend(loc='upper right',numpoints=1)  
+    plt.grid(True)
     
     plt.subplot(2, 1, 2)
     plt.errorbar(T, s_avg/Nsp[j], yerr=s_std/Nsp[j], fmt=forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel(u'Suceptibilidad magnética')
-    plt.ylim([-4 , 140])
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$<\chi>/N$')
+    plt.ylim([-4 , 280])
     plt.legend(loc='upper right',numpoints=1)  
+    plt.grid(True)
     
     plt.figure(3)
     
     plt.errorbar(T,a_avg,yerr=a_std,fmt=forma[j],label=leg[j])
-    plt.xlabel('Temperatura')
-    plt.ylabel('Aceptaciones [%]')
+    plt.xlabel('$kT/J$')
+    plt.ylabel('$Aceptaciones [\%]$')
     plt.ylim([-5 , 75])
     plt.legend(loc='upper left',numpoints=1)
+    plt.grid(True)
     
     j += 1
 
