97f62992e1e68f666b1a44d75b0d5908f3612270
Pablo Bellino <pbellino@gmail.com>
2015-10-07 07:45:42 -0300

    Títulos y labels para los gráficos temporales

--- FORTRAN/ising/src/grafico_temporal.py
+++ FORTRAN/ising/src/grafico_temporal.py
@@ -18,18 +18,25 @@ E = np.loadtxt(arch_eng)
 M = np.loadtxt(arch_mag)
 
 
-plt.figure(1)
+plt.figure(1,figsize=(8,8))
 
 plt.subplot(2, 1, 1)
-plt.plot(E/400, 'b.')
-plt.title('Datos temporales')
-plt.xlabel('Tiempo [u.a.]')
-plt.ylabel(u'Energía')
+plt.plot(E/400, 'k.')
+#plt.title('Datos temporales')
+plt.xlabel('$Paso\,MC$')
+plt.ylabel(u'$E/N$')
+plt.ylim(-2.1,plt.ylim()[1])
+plt.grid(True)
 
 plt.subplot(2, 1, 2)
-plt.plot(M/400, 'b.')
-plt.xlabel('Tiempo [u.a.]')
-plt.ylabel(u'Magnetización')
+plt.plot(M/400, 'k.')
+plt.xlabel('$Paso\,MC$')
+plt.ylabel(u'$M/N$')
+if M[-1]/400 < 0:
+    plt.ylim(-1.1,plt.ylim()[1])
+else:
+    plt.ylim(plt.ylim()[0],1.1)
 
+plt.grid(True)
 
 plt.show()
