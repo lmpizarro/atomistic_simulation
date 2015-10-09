cf71ff85c05ce897e8c1bd9e05a7a643e3e2e5c5
Luis María Pizarro <lmpizarro@gmail.com>
2015-10-09 11:05:26 -0300

    constants.f90 para mantener valores de constantes importantes agregados

--- FORTRAN/dinmod/src/dinmod.f90
+++ FORTRAN/dinmod/src/dinmod.f90
@@ -1,12 +1,14 @@
 program main_dimod 
 
-  use io_parametros, only: read_parameters
-  use globales, only: gT, gDt, gL, gNpart, gNtime               
+    use io_parametros, only: read_parameters
+    use globales, only: gT, gDt, gL, gNpart, gNtime
+    use constants, only: kb
+  
 
-  implicit none
+    implicit none
 
-  ! Lee los datos necesario
-  call read_parameters()
+    ! Lee los datos necesario
+    call read_parameters()
 
 
 end program main_dimod
