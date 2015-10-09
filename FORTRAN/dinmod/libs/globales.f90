1a5e16277484872f97213d7aa7efa1fe49613857
Luis María Pizarro <lmpizarro@gmail.com>
2015-10-09 10:37:38 -0300

    basico read_parameters

--- FORTRAN/dinmod/libs/globales.f90
+++ FORTRAN/dinmod/libs/globales.f90
@@ -3,8 +3,13 @@ module globales
   implicit none
 
   integer, parameter :: dp=kind(0.d0)
+  ! gT: Temperatura de la corrida
+  ! gDt: Paso de tiempo de la corrida
+  ! gL: longitud de un lado del cubo
   real(dp) :: gT, gDt, gL
+  ! gNpart: cantidad de partículas del sistema
+  ! gNtime: cantidad de pasos de  tiempo
   integer :: gNpart, gNtime
-
+  !real(dp),  dimension(:), allocatable:: 
 
 end module globales
--- FORTRAN/dinmod/libs/io_parametros.f90
+++ FORTRAN/dinmod/libs/io_parametros.f90
@@ -1,5 +1,5 @@
 module io_parametros
-  use globales,     only: dp
+  use globales,     only: dp, gT, gDt, gL, gNpart, gNtime
 
   implicit none
 
@@ -11,7 +11,22 @@ contains
 !===============================================================================
 
   subroutine read_parameters()
- 
+        logical :: es
+     
+        inquire(file='./parameters.dat',exist=es)
+        if(es) then
+            open(unit=10,file='./parameters.dat',status='old')
+            read(10,*) gT, gNpart, gL, gDt, gNtime
+            close(10)
+        else
+            print *, "Usando parametros por default"
+            gT = 293
+            gNpart = 1000
+            gL = 10
+            gDt = .1
+            gNtime = 100
+        end if
+        print *,"* read_parameters ", gT, gNpart, gL, gDt, gNtime 
 
   end subroutine read_parameters
 
