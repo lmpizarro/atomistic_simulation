88141008c44e5a51b24f5ac470cda86ced45b5d1
Pablo Bellino <pbellino@gmail.com>
2015-10-13 09:09:56 -0300

    Se agrega subrutina para calcular la fuerza. Con radio de corte y CPC

--- FORTRAN/dinmod/libs/dinmods.f90
+++ FORTRAN/dinmod/libs/dinmods.f90
@@ -1,4 +1,5 @@
 module dinmods 
+
     use types, only: dp
     use globales, only: gT, gDt, gL, gNpart, gNtime, gR, gF, gV, sigma, epsil
     use utils, only: write_array3D_lin
@@ -8,23 +9,29 @@ module dinmods
     implicit none
 
     private
-    public :: inicializacion, potencial, inicia_posicion_cs, finalizacion, cpc
+
+    public :: inicializacion, potencial, inicia_posicion_cs, finalizacion, cpc, fuerza
 
 contains
+
     !===============================================================================
     ! INICIALIZA SISTEMA de CALCULO
     !===============================================================================
     subroutine inicializacion()
+
         allocate(gR(3,gNpart))
         allocate(gV(3,gNpart))
         allocate(gF(3,gNpart))
+
         call inic_zig()
+
     end subroutine inicializacion 
 
    
     !===============================================================================
     ! Condiciones períodicas de contorno
     !===============================================================================
+
     subroutine cpc( l)
             integer, intent(in) :: l
 
@@ -57,6 +64,7 @@ contains
     !===============================================================================
     ! INICIALIZA Posicion en Red periódica cúbica simple
     !===============================================================================
+
     subroutine inicia_posicion_cs()
         integer :: i, j, k, l, nl
         real(dp) :: rx, ry, rz
@@ -124,13 +132,75 @@ contains
             enddo
         enddo
         v = 4.0 * epsil * v
+
     endfunction potencial
+
+    !===============================================================================
+    ! CALCULA LA FUERZA  
+    !===============================================================================
+
+    subroutine fuerza
+
+    real(dp), dimension(3)  :: rij_vec   ! Distancia vectorial entre i y j
+    real(dp)                :: r2ij      ! Módulo cuadrado de la distancia rij
+    real(dp)                :: Fij       ! Módulo fuerza entre partículas i y j
+    real(dp)                :: Pot       ! Energía potencial
+    real(dp)                :: rc2       ! Cuadrado del radio de corte
+    real(dp)                :: pot_cut   ! Potencial L-J en el radio de corte
+    real(dp)                :: r2in,r6in ! Inversa distancia rij a la 2 y 6
+    integer                 :: i,j       
+
+    rc2 = (2.5_dp)**2                ! Definición provisoria del radio de corte
+    pot_cut = 4*epsil*( 1/(rc2**6) - 1/(rc2**3) ) 
+
+      ! Se van a acumular las fuerzas. Se comienza poniendo todas a cero.
+      gF  = 0.0_dp
+      Pot = 0.0_dp
+      ! Paso a trabajar ditancias en unidades de sigma
+      gR = gR/sigma
+      gL = gL/sigma
+
+      do i = 1, gNpart - 1       
+        do j = i+1, gNpart
+          rij_vec = gR(:,i) - gR(:,j)               ! Distancia vectorial
+          ! Si las partícula está a más de gL/2, la traslado a r' = r +/- L
+          ! Siempre en distancias relativas de sigma
+          rij_vec = rij_vec - gL*nint(rij_vec/gL)
+          r2ij   = dot_product( rij_vec , rij_vec )    ! Cuadrado de la distancia
+          if ( r2ij < rc2 ) then               
+            r2in = 1/r2ij                              ! Inversa al cuadrado
+            r6in = r2in**3                             ! Inversa a la sexta
+            Fij     = r2in * r6in * (r6in - 0.5_dp)    ! Fuerza entre partículas
+            gF(:,i) = gF(:,i) + Fij * rij_vec          ! Contribución a la partícula i
+            gF(:,j) = gF(:,j) - Fij * rij_vec          ! Contribucion a la partícula j
+            Pot     = Pot + r6in * ( r6in - 1.0_dp)    ! Energía potencial
+          end if
+        end do
+      end do
+
+      ! Constantes que faltaban en la energía
+      Fij = 48 * epsil * Fij                
+      ! Constantes que faltaban en el potencial
+      ! Agrego el desplazamiento del potencial considerando la cantidad de
+      ! pares con que se obtuvo la energía potencial N(N-1)/2
+      Pot =  4 * epsil * Pot - gNpart*(gNpart-1)*pot_cut/2  
+ 
+      ! Se vuelven a pasar a las coordenadas absolutas
+      gR = gR*sigma
+      gL = gL*sigma
+
+      write(*,'(A,2X,3(E15.5,3X))')  'Sumatoria de fuerzas:' , sum(gF,2)
+      print *, Pot
+
+    end subroutine fuerza 
 
     !===============================================================================
     ! FINALIZA PARAMETROS
     !===============================================================================
     subroutine finalizacion()
-        call fin_zig()
+
+      call fin_zig()
+
     endsubroutine finalizacion
 
 
