74ba47309668754a035c864c4218afaefb2aa348
Luis María Pizarro <lmpizarro@gmail.com>
2015-10-13 10:37:57 -0300

    eliminada la funcion de calculo de potencial de dinmods.f90

--- FORTRAN/dinmod/libs/dinmods.f90
+++ FORTRAN/dinmod/libs/dinmods.f90
@@ -99,41 +99,7 @@ contains
                 rz = rz + 1
             end if        
         enddo     
-        call write_array3D_lin (gR)
-        print *, "potencial", potencial()
-        !call metropolis ()
     end subroutine inicia_posicion_cs
-
-    ! calculo del potencial de pag 18 del allen-tildesley
-    function potencial () result(v)
-        integer :: i, j     
-        real(dp) :: v, rxi, ryi, rzi
-        real(dp) :: rxij, ryij, rzij, rijsq
-        real(dp) :: sr2, sr6, sr12
-
-        v = 0
-
-        do i = 1, gNpart - 1
-
-            rxi = gR(1, i)
-            ryi = gR(2, i)
-            rzi = gR(3, i)
-
-            do j = i + 1, gNpart
-                rxij = rxi - gR(1,j)
-                ryij = ryi - gR(2,j)
-                rzij = rzi - gR(3,j)
-    
-                rijsq = rxij ** 2 + ryij ** 2 + rzij ** 2
-                sr2 = sigma / rijsq
-                sr6 = sr2 **3
-                sr12 = sr6 ** 2
-                v = v + sr12 - sr6
-            enddo
-        enddo
-        v = 4.0 * epsil * v
-
-    endfunction potencial
 
     !===============================================================================
     ! CALCULA LA FUERZA  
