f3d5c746d29bd920f80901e7ed81e3fd003f97ac
Luis María Pizarro <lmpizarro@gmail.com>
2015-10-14 12:53:02 -0300

    agregada la k_b en unidades de KJ

--- FORTRAN/dinmod/libs/constants.f90
+++ FORTRAN/dinmod/libs/constants.f90
@@ -2,9 +2,10 @@ module constants
 
      use types, only :dp
      real(dp), parameter ::                &
-        PI           = 3.1415926535898_8, & ! pi
-        N_AVOGADRO   = 0.602214129_8,     & ! Avogadro's number in 10^24/mol
-        K_BOLTZMANN  = 8.6173324e-11_8,   &
-        kb           = 1.0
+        PI           = 3.1415926535898_dp, & ! pi
+        N_AVOGADRO   = 0.602214129_dp,     & ! Avogadro's number in 10^24/mol
+        K_BOLTZMANN  = 8.6173324e-11_dp,   & ! ev K-1
+        kb           = 1.0_dpi,            &
+        K_B_KJ       = 1.3806488e-26_dp      ! KJ K-1
 
 end module constants
