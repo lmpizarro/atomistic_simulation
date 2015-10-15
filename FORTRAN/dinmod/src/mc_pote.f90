f97e315e51eb922a73c0687f10100e4bdf345cc9
lpizarro <lpizarro@cnea.gov.ar>
2015-10-15 09:25:47 -0300

    comparar resultados de potencial de pbellino y lmpizarro

--- FORTRAN/dinmod/src/mc_pote.f90
+++ FORTRAN/dinmod/src/mc_pote.f90
@@ -1,18 +1,26 @@
 program main_mc_pote 
 
+    use types
     use io_parametros, only: read_parameters
     use dinmods
     use mc
+    use potenciales
   
 
     implicit none
+
+    real(dp) :: pot 
 
     ! Lee los datos necesario
     call read_parameters()
     call inicializacion()
     call inicia_posicion_rn()
 
-    call metropolis()
+    !call metropolis()
+
+    pot = poten_lj_vec() 
+
+    print *, "potenciales lj vec", pot
 
     call finalizacion()
 
