314ea6f92e03418ba8e386b71e1b01a45ef54d63
Pablo Bellino <pbellino@gmail.com>
2015-10-15 10:36:02 -0300

    Se modifica error en CMaleList.txt. No estaba tomando los flags del compilador. Se define la versión release por default

--- FORTRAN/dinmod/src/dinmod.f90
+++ FORTRAN/dinmod/src/dinmod.f90
@@ -11,6 +11,14 @@ program main_dimod
 
     implicit none
 
+    integer   :: n_core, n_thread
+
+    !n_core   = omp_get_num_procs()
+    !n_thread = omp_get_max_threads()
+
+    write(*,*) 'Número de procesadores: ', n_core
+    write(*,*) 'Número de threads disponibles: ', n_thread
+
     ! Lee los datos necesario
     call read_parameters()
     ! Inicializa parámetros 
