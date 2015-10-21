b9de939ecbb47fab56129f727bb7470429b84395
Pablo Bellino <pbellino@gmail.com>
2015-10-21 15:07:31 -0300

    Todas las subrutinas de medición de variables físicas se pasan al modulo mediciones.f90

--- FORTRAN/dinmod/libs/dinmods.f90
+++ FORTRAN/dinmod/libs/dinmods.f90
@@ -1,8 +1,10 @@
 module dinmods 
 
   use types,      only: dp
-  use globales,   only: gT, gDt, gL, gNpart, gNtime, gR, gF, gV, gSigma, gEpsil, gM, gNmed
+  use globales,   only: gT, gDt, gL, gNpart, gNtime, gR, gF, gV, gSigma, gEpsil, gM, & 
+                        gNmed, gRc2, gPot_cut 
   use utils,      only: write_array3D_lin, init_openmp
+  use mediciones, only: calcula_kin, calcula_pres, calcula_fuerza 
   use constants,  only: PI
   use ziggurat
   use usozig
@@ -32,10 +34,10 @@ module dinmods
 
   real(dp)        :: Pot       ! Energía potencial del sistema
   real(dp)        :: Kin       ! Energia cinetica del sistema
-  real(dp)        :: rc2       ! Cuadrado del radio de corte
-  real(dp)        :: pot_cut   ! Potencial L-J en el radio de corte
   real(dp)        :: virial    ! Cálculo del virial para la presión 
-  
+  real(dp)        :: Vol       ! Volumen del cubo
+  real(dp)        :: rho       ! Densidad numero de particulas N/V
+
 contains
 
   !===============================================================================
@@ -60,6 +62,16 @@ contains
     allocate(gV(3,gNpart))
     allocate(gF(3,gNpart))
 
+    ! Calcula volumen y densidad
+    Vol = gL**3
+    rho = gNpart / Vol
+
+    write(*,'(a)') ''
+    write(*,'(a)')      '*********** PARAMETROS DERIVADOS ************'
+    write(*,'(a,F8.3)') '************ Volumen               = ' , Vol 
+    write(*,'(a,F8.4)') '************ Densidad              = ' , rho
+    write(*,'(a)')      '*********************************************'
+
     ! Inicial generador de número aleatorios
     call inic_zig()
     ! Define el radio de corte y el potencial desplazado
@@ -74,13 +86,13 @@ contains
       ! Busca el mínimo de energía
       write(*,*) '* Se busca un mínimo de energía potencial'
       call integracion_min()
-      ! Define velocidades iniciales
+      ! DKinefine velocidades iniciales
       call vel_inic()
     end if
     ! Calcula energía cinética inicial 
-    call calcula_kin()
+    call calcula_kin(Kin)
     ! Calcula la fuerza inicial
-    call fuerza()
+    call calcula_fuerza(Pot,virial)
 
     print *, '* Valores iniciales por partícula'
     print *, 'Pot=', Pot/gNpart, 'Kin=' , Kin/gNpart, 'Tot=', (Pot+Kin)/gNPart
@@ -95,14 +107,14 @@ contains
   subroutine corta_desplaza_pote()   
 
     ! Radio de corte fijo
-    rc2 = (2.5_dp)**2                
+    gRc2 = (2.5_dp)**2                
     ! Potencial de L-J evaluado en el radio de corte
-    pot_cut = 4.0_dp*gEpsil*( 1.0_dp/(rc2**6) - 1.0_dp/(rc2**3) ) 
+    gPot_cut = 4.0_dp*gEpsil*( 1.0_dp/(gRc2**6) - 1.0_dp/(gRc2**3) ) 
     ! Imprime en pantalla
     write(*,'(a)')      ''
     write(*,'(a)')      '*************** RADIO DE CORTE **************'
-    write(*,'(a,F9.4)') '************ Rc    = ' , sqrt(rc2)
-    write(*,'(a,F9.5)') '************ V(Rc) = ' , pot_cut
+    write(*,'(a,F9.4)') '************ Rc    = ' , sqrt(gRc2)
+    write(*,'(a,F9.5)') '************ V(Rc) = ' , gPot_cut
     write(*,'(a)')      '*********************************************'
 
   end subroutine corta_desplaza_pote
@@ -238,152 +250,6 @@ contains
   end subroutine inicia_posicion_rn
 
   !===============================================================================
-  ! CALCULA LA FUERZA  
-  !===============================================================================
-  ! Calcula la fuerza ya la energía potencial del potencial de L-J
-
-  subroutine fuerza()
-
-    real(dp), dimension(3)  :: rij_vec   ! Distancia vectorial entre i y j
-    real(dp)                :: r2ij      ! Módulo cuadrado de la distancia rij
-    real(dp)                :: Fij       ! Módulo fuerza entre partículas i y j
-    real(dp)                :: r2in,r6in ! Inversa distancia rij a la 2 y 6
-    integer                 :: i,j       
-
-!$omp parallel workshare
-    ! Se van a acumular las fuerzas. Se comienza poniendo todas a cero.
-    gF     = 0.0_dp
-    Pot    = 0.0_dp
-    virial = 0.0_dp
-    ! Paso a trabajar distancias en unidades de sigma
-    gR = gR/gSigma
-    gL = gL/gSigma
-!$omp end parallel workshare
-
-! Se escribe dos loops distintos dependiendo de si se compila el programa con
-! OPENMP o no. La razón es que para paralelizar correctamente el loop de fuerza
-! se debe cambiar la forma de recorrer las interacciones i,j en vez de j<i
-! Esto implicaría peor performance en caso de no utilizar openmp.
-! Por ejemplo, para Npart = 100, Ntime = 10000
-! los tiempos serían:  sin openmp  - 2.8
-!                      1 thread    - 5.0
-!                      2 threads   - 2.6
-!                      4 threads   - 1.5
-#ifdef _OPENMP
-! Si se compila con OPENMP
-! Se usa un loop corriendo sobre todas los ij. Se calcula la fuerza sólo una vez y se
-! divide el potencial por 1/2 para cada partícula
-
-!$omp parallel &
-!$omp shared (gNpart, gR, gL, rc2, gF ) &
-!$omp private (i, j, rij_vec, r2ij, r2in, r6in, Fij)
-
-!$omp do reduction( + : Pot)
-
-     do i = 1, gNpart       
-      do j = 1, gNpart
-        if (i /= j) then
-          rij_vec = gR(:,i) - gR(:,j)               ! Distancia vectorial
-          ! Si las partícula está a más de gL/2, la traslado a r' = r +/- L
-          ! Siempre en distancias relativas de sigma
-          rij_vec = rij_vec - gL*nint(rij_vec/gL)
-          r2ij   = dot_product( rij_vec , rij_vec )    ! Cuadrado de la distancia
-          if ( r2ij < rc2 ) then               
-            r2in = 1.0_dp/r2ij                         ! Inversa al cuadrado
-            r6in = r2in**3                             ! Inversa a la sexta
-            Fij     = r2in * r6in * (r6in - 0.5_dp)    ! Fuerza entre partículas
-            gF(:,i) = gF(:,i) + Fij * rij_vec          ! Contribución a la partícula i
-            Pot     = Pot + r6in * ( r6in - 1.0_dp)    ! Energía potencial
-            virial  = virial + Fij * r2ij              ! Término del virial para la presión
-                                                       ! pg 48 de Allen W=-1/3 sum(r dv/dr)
-          end if
-        end if
-      end do
-    end do
-
-    Pot = 0.5_dp * Pot       ! En este loop se cuentan dos veces las interacciones
-    virial = 0.5_dp * virial ! En este loop se cuentan dos veces las interacciones
-
-!$omp end do
-!$omp end parallel
-
-#else
-! Si no se compila con OPENMP
-! Se usa un loop corriendo sólo sobre los j<i. Se asigna la fuerza a dos partículas
-! con signo contrario. Se calcula el potencial por cada interacción (sin repetir)
-
-    do i = 1, gNpart - 1       
-      do j = i+1, gNpart
-        rij_vec = gR(:,i) - gR(:,j)               ! Distancia vectorial
-        ! Si las partícula está a más de gL/2, la traslado a r' = r +/- L
-        ! Siempre en distancias relativas de sigma
-        rij_vec = rij_vec - gL*nint(rij_vec/gL)
-        r2ij   = dot_product( rij_vec , rij_vec )    ! Cuadrado de la distancia
-        if ( r2ij < rc2 ) then               
-          r2in = 1.0_dp/r2ij                         ! Inversa al cuadrado
-          r6in = r2in**3                             ! Inversa a la sexta
-          Fij     = r2in * r6in * (r6in - 0.5_dp)    ! Fuerza entre partículas
-          gF(:,i) = gF(:,i) + Fij * rij_vec          ! Contribución a la partícula i
-          gF(:,j) = gF(:,j) - Fij * rij_vec          ! Contribucion a la partícula j
-          Pot     = Pot + r6in * ( r6in - 1.0_dp)    ! Energía potencial
-          virial  = virial + Fij * r2ij              ! Término del virial para la presión
-                                                     ! pg 48 de Allen W=-1/3 sum(r dv/dr)
-        end if
-      end do
-    end do
-
-#endif
-
-!$omp parallel workshare
-    ! Constantes que faltaban en la energía
-    gF = 48.0_dp * gEpsil * gF / gSigma                
-    ! Constantes que faltaban en el potencial
-    ! Agrego el desplazamiento del potencial considerando la cantidad de
-    ! pares con que se obtuvo la energía potencial N(N-1)/2
-    Pot =  4.0_dp * gEpsil * Pot - gNpart*(gNpart-1)*pot_cut/2.0_dp  
-    ! Se agregan las constantes que faltan para el término del virial
-    virial = 48.0_dp * gEpsil * virial / 3.0_dp
-
-    ! Se vuelven a pasar a las coordenadas absoluta
-    gR = gR*gSigma
-    gL = gL*gSigma
-!$omp end parallel workshare
-
-!    write(*,'(A,2X,3(E15.5,3X))')  'Sumatoria de fuerzas:' , sum(gF,2)
-!    print *, 'Potencial: ', Pot
-
-  end subroutine fuerza 
- 
-  !===============================================================================
-  ! CALCULA ENERGIA CINETICA 
-  !===============================================================================
-  ! Calcula la anergia cinetica total del sistema
-
-  subroutine calcula_kin()
-    
-    real(dp), dimension(gNpart)   :: v2    ! Vector con la velocidad cuadratica
-    integer                       :: i
-
-!$omp parallel &
-!$omp shared( gNpart, v2, gV) &
-!$omp private ( i )
-!$omp do
-    do i = 1, gNpart
-      v2(i) =  dot_product(gV(:,i),gV(:,i))  
-    end do
-!$omp end do
-!$omp end parallel
-
-!$omp parallel &
-!$omp shared (Kin, gM, v2)
-!$omp workshare
-    Kin = 0.5_dp * gM * sum( v2 )
-!$omp end workshare
-!$omp end parallel
-
-  end subroutine
-
-  !===============================================================================
   ! INTEGRACIÓN DE LAS ECUACIONES DE MOVIMIENTO - MINIMIZACIÓN ENERGÍA
   !===============================================================================
   ! Subrutina de integración de las ecuaciones de movimiento para minimizar energía
@@ -406,7 +272,7 @@ contains
       ! Esta subrutine abre y cierra un archivo. Se puede optimizar haciéndolo acá.
       call escribe_trayectoria(gR,i)    
       ! Calcula fuerza y energía
-      call fuerza()
+      call calcula_fuerza(Pot,virial)
       ! Escribe energía potencial en vector
       Eng_t(i+1) = Pot
     end do
@@ -440,14 +306,14 @@ contains
     print *, 'Pot=' , Pot/gNpart, 'Kin=', Kin/gNpart, 'Tot=', (Pot+Kin)/gNpart
 
     do i = 1, gNtime 
-      gR = gR + gDt*gV + 0.5_dp * gF * gDt**2 / gM           ! gR(t+dt)
-      gV =          gV + 0.5_dp * gF * gDt / gM              ! gV(t+0.5dt) 
-      call fuerza()                                          ! Calcula fuerzas y potencial
-      gV =          gV + 0.5_dp * gF * gDt / gM              ! gV(t+dt)
+      gR = gR + gDt*gV + 0.5_dp * gF * gDt**2 / gM        ! gR(t+dt)
+      gV =          gV + 0.5_dp * gF * gDt / gM           ! gV(t+0.5dt) 
+      call calcula_fuerza(Pot,virial)                     ! Calcula fuerzas y potencial
+      gV =          gV + 0.5_dp * gF * gDt / gM           ! gV(t+dt)
       ! Aplica condiciones peródicas de contorno
       call cpc_vec()
       ! Calcula la energia cinetica
-      call calcula_kin()
+      call calcula_kin(Kin)
       ! Escribe energía total
       Eng_t(i+1) = Pot + Kin
       ! Escribe posiciones de las partículas
@@ -465,7 +331,6 @@ contains
     print *, '* Fin de la integracion temporal'
 
   end subroutine integracion
-
 
   !===============================================================================
   ! FINALIZA PARAMETROS
