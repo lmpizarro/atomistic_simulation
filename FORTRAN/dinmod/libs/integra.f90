60e5be77bc5e702f3bc42a27d280589a3e33411b
Pablo Bellino <pbellino@gmail.com>
2015-10-26 14:43:08 -0300

    Se modifica la forma en que se hacen las mediciones y se guardan los datos. Nmed puede ser negativo indicando que no se guardarán los datos

--- FORTRAN/dinmod/libs/integra.f90
+++ FORTRAN/dinmod/libs/integra.f90
@@ -20,21 +20,27 @@ contains
 
   subroutine integracion()
 
-    real(dp)        :: Pres      ! Presión instantánea
-
-!TODO Ver cómo escbirir energías y presiones. Ahora sólo anda si Nmed=1
-    real(dp), dimension(3,gNtime+1)   :: Eng_t   ! Energía en función del tiempo
-    real(dp), dimension(gNtime+1)   :: Pres_t  ! Presión en función del tiempo
+    real(dp)                                :: Pres    ! Presión instantánea
+    real(dp), dimension(:,:), allocatable   :: Eng_t   ! Energía en función del tiempo
+    real(dp), dimension(:), allocatable     :: Pres_t  ! Presión en función del tiempo
     integer    :: i, j
+    integer    :: Kmed                                 ! Cantidad de puntos medidos
 
+    ! Se define la cantidad de puntos que se van a medir
+    Kmed = int(gNtime/abs(gNmed)) + 1              ! Se agrega +1 para poner el inicial
+    allocate(Eng_t(1:3,1:Kmed),Pres_t(1:Kmed))
+
+    ! El primer punto son los valores iniciales
+    call calcula_pres(Pres)
     Pres_t(1) = Pres
-    ! El primer punto es la energía inicial
     Eng_t(:,1) = (/ gPot, gKin, gPot + gKin/) 
+    ! Imprime en pantalla info
     write(*,*) '********************************************'
     print *, '* Comienza integracion temporal (Vel-Verlet)'
     print *, '* Energias por partícula al comienzo de la integración'
     call print_info(Pres)
 
+    j = 1                                              ! Contador para el loop de mediciones
     do i = 1, gNtime 
       gR = gR + gDt*gV + 0.5_dp * gF * gDt**2 / gM     ! gR(t+dt)
       ! Aplica condiciones peródicas de contorno
@@ -49,26 +55,29 @@ contains
         ! Presión
         call calcula_pres(Pres)
         ! Escribe energía total
-        Eng_t(:,i+1) = (/gPot, gKin, gPot + gKin/) 
-        Pres_t(i+1) = Pres
+        Eng_t(:,j) = (/gPot, gKin, gPot + gKin/) 
+        Pres_t(j)  = Pres
         ! Escribe posiciones de las partículas
         !call escribe_trayectoria(gR,i)
+        j = j + 1                                      ! Actualiza contador mediciones
       end if
     end do
 
-    ! Guarda la energía potencial en un archivo
-    open(unit=10,file='./energia_tot.dat',status='unknown')
-    !write(10,'(F10.4)') gDt
-    do j= 1,gNtime+1
-      write(10,'(4(E16.9,2X))') (/ (j-1)*gDt, ( Eng_t(i,j)/gNpart, i=1,3) /)
-    end do
-    close(10)
+    if (gNmed > 0) then
+      ! Guarda la energía potencial en un archivo
+      open(unit=10,file='./energia_tot.dat',status='unknown')
+      !write(10,'(F10.4)') gDt
+      do j= 1,Kmed
+        write(10,'(4(E16.9,2X))') (/ (j-1)*gDt, ( Eng_t(i,j)/gNpart, i=1,3) /)
+      end do
+      close(10)
 
-    ! Guarda la presion en un archivo
-    open(unit=20,file='./presion.dat',status='unknown')
-    !write(10,'(F10.4)') gDt
-    write(20,'(E16.9)') Pres_t 
-    close(20)
+      ! Guarda la presion en un archivo
+      open(unit=20,file='./presion.dat',status='unknown')
+      !write(10,'(F10.4)') gDt
+      write(20,'(E16.9)') Pres_t 
+      close(20)
+    end if
     
     ! En caso de que no se hagan mediciones, se calculan los valores finales
     call calcula_kin()
@@ -77,6 +86,8 @@ contains
     print *, '* Energias por partícula al final de la integración'
     call print_info(Pres)
     print *, '* Fin de la integracion temporal'
+    ! Se libera memoria
+    deallocate(Eng_t,Pres_t)
 
 contains
 
