7411b02bf819df0acc8be2a5f4abcd8cc3f9a664
Luis María Pizarro <lmpizarro@gmail.com>
2015-10-30 16:19:37 -0300

    gaph_data revisado

--- FORTRAN/dinmod/scripts/graph_data.py
+++ FORTRAN/dinmod/scripts/graph_data.py
@@ -1,9 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+N = 500
+a = np.loadtxt("presion.030.txt")
 
-a = np.loadtxt("data.txt")
+print np.mean(a[N:]), np.var(a[N:]), np.std(a[N:])
 
-plt.plot(a)
+plt.plot(a[N:])
 
+plt.show()
+
+
+hist, bins = np.histogram(a[N:], bins=50)
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+plt.bar(center, hist, align='center', width=width)
 plt.show()
