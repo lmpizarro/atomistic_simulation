a9f060f696e8e696d526ff4af442bb790c341578
Pablo <pbellino@gmail.com>
2015-11-01 01:14:46 -0300

    prepro e instalacion

--- FORTRAN/dinmod/src/control.h
+++ FORTRAN/dinmod/src/control.h
@@ -1,8 +1,8 @@
 /* Preprocessor variables and directives */
 /* Para verificar lel correcto funcionamiento del termostato */
 /* Guarda datos de velocidades para sacar histograma */
-#define CONTROL_TEMP
+#undef CONTROL_TEMP
 /* when not defined, better to do: #undef VAR */
 #define THERM 1 /*0=NVE, no thermostat 1=langevin thermostat */
 /* Defino variable para indicar si se graban las trayectorias de las partículas */
-#define GRABA_TRAYECTORIA
+#undef GRABA_TRAYECTORIA
