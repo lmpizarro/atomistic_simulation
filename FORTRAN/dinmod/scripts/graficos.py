e65184add032fc15320e2577d58aeaf0592ea618
Pablo <pbellino@gmail.com>
2015-11-07 19:14:58 -0300

    Graficación genérica para archivos con una y dos columnas

--- FORTRAN/dinmod/scripts/graficos.py
+++ FORTRAN/dinmod/scripts/graficos.py
@@ -6,14 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+plt.figure(1,figsize=(8,6))
+
 arch = sys.argv[1]
 
 y = np.loadtxt(arch,skiprows=1)
 
-plt.figure(1,figsize=(8,6))
+if len(y.shape) > 1:
+    columnas = y.shape[1]
+else:
+    columnas = 1
 
-plt.plot(y, 'k.')
-plt.title('Archivo = ' + arch)
+if columnas == 2:
+    x = y[:,0]
+    y = y[:,1] 
+    plt.plot(x,y, 'k.')
+else:
+    plt.plot(y, 'k.')
+
+
+plt.title('Archivo leido: ' + arch)
 #plt.xlabel('$Tiempo$')
 #plt.ylabel(u'$Presión$')
 #plt.ylim(-2.1,plt.ylim()[1])
