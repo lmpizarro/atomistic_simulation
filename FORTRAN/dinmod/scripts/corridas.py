d5e827ddabcd701c4dc2300d1f2f1804a93da669
Pablo <pbellino@gmail.com>
2015-11-08 19:15:21 -0300

    Se comienza a armar estructura de archivos para realizar las corridas en python. Copiadas de cómo se hizo en el programa de Ising

--- FORTRAN/dinmod/scripts/corridas.py
+++ FORTRAN/dinmod/scripts/corridas.py
@@ -4,7 +4,21 @@
 
 ###############################################################################
 #
-# Script para correr el programa de dinámica molecular en serie
+# Script para correr el programa de ising a distintas temperaturas
+# Crea una carpeta para cada temperatura. En cada una de esas carpetas, a su
+# vez, crea Nruns carpetas para hacer estadística y obtener los valores con
+# sus respectivos errores.
+#
+# En cada temperatura, se utiliza el valor final de la temperatura anterior.
+# Arbitrariamente, se toma el valor final de RUN00 como el valor inicial de
+# todas las corridas a la siguiente temperatura (menor)
+#
+# Los resultados los guarda en el archivo 'tablas_temperatura.dat'
+# Cada fila de 'tablas_temperatura.dat' representa los resultados a una dada
+# temperatura con sus respectivos errores. Las columnas son:
+# T <E> std(E) <M> std(M) <c> std(c) <suc> std(suc) <acept> std(acept)
+# Los errores se calculan como std(RUN)/sqrt(NRUN)
+###############################################################################
 
 import os
 import os.path
@@ -13,4 +27,190 @@ import subprocess
 import errno
 import numpy as np
 
+# Funciones propias para este script
+import ising_fun as isf
+
+###############################################################################       
+#   PARAMETROS DE ENTRADA
+###############################################################################
+
+if os.path.isfile("parametros.py"):
+   
+    import parametros
+    
+    N_red = parametros.N_red
+    M_red = parametros.M_red
+    T_min = np.float(parametros.T_min)
+    T_max = np.float(parametros.T_max)
+    dT = np.float(parametros.dT)
+    T_detail_min = np.float(parametros.T_detail_min)
+    T_detail_max = np.float(parametros.T_detail_max)
+    dT_detail = np.float(parametros.dT_detail)
+    N_term  = parametros.N_term
+    N_medi  = parametros.N_medi
+    Nrun = parametros.Nrun
+    N_grab = parametros.N_grab
+else:
+    ## Tamaño de la red de spines
+    N_red = 20
+    M_red = 20
+    # Cada cuántos puntos se quiere grabar el archivo temporal
+    N_grab = 0 
+   
+    # Barrido de temperaturas
+    # Temperatura mínima
+    T_min = np.float(1.5)
+    # Temperatura máxima
+    T_max = np.float(1.7)
+    # Paso de temperatura
+    dT = np.float(0.1)
+    # Agrego el detalle cerca de la temperatura crítica
+    T_detail_min = np.float(2.15)
+    T_detail_max = np.float(2.45)
+    dT_detail = np.float(0.03)
+    # Número de pasos para la primer corrida (termalización)
+    N_term = '4000'
+    # Número de pasos para la segunda corrida (medición)
+    N_medi = '10000'
+    # Número de corridas para cada temperatura
+    Nrun = 10
+    
+# Escribe los valores al archivo parametros.dat
+# Especifica tamaño de la red de spines
+isf.escribe_entrada('N_red',str(N_red))
+isf.escribe_entrada('M_red',str(M_red))
+# Especifica cada cuanto se graban datos temporales 
+isf.escribe_entrada('N_grab',str(N_grab))
+
+#
+# FIN PARAMETROS DE ENTRADA
+###############################################################################
+
+# Lista de temperaturas de la zona de detalle
+detalle = np.arange(T_detail_min,T_detail_max + dT_detail, dT_detail)
+
+# Lista de temperaturas de paso grueso
+tempe = np.arange(T_min,T_max+dT,dT)
+
+tempe = np.append(tempe, detalle)
+# filtra por valores unicos de temperatura
+# se debe redondear (con algún criterio) para evitar falsas duplicaciones
+# por errores de redondeo de python
+tempe = np.unique(tempe.round(decimals=6))
+# ordena de mayor a menor el array numpy
+tempe = np.fliplr([tempe])[0]
+
+# Número de corridas para las T fuera de la zona crítica
+N_run_base = Nrun
+
+# Directorio raíz donde está el ejecutable y este script
+curr_dir = os.getcwd()
+
+###############################################################################
+# LOOP QUE CORRE SOBRE TODAS LAS TEMPERATURAS
+###############################################################################
+T_anterior = []    # Buffer para copiar el estado final a T anterior
+                   # al estado actual.
+for T in tempe:
+    Tnombre = str(T)
+    # Nombre de la carpeta uqe se va a crear
+    carpeta = Tnombre + '_tmpfolder'
+    # Camino completo de la carpeta que se va a crear
+    path_carpeta = os.path.join(curr_dir,carpeta)
+    # Se crea la carpeta sólo si ésta no existe. De lo contrario se saltea la 
+    # temperatura correspondiente
+    try: 
+        os.makedirs(path_carpeta)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+        else:
+            print('Ya existe el directorio. Se omite corrida con T=' + Tnombre)
+            continue
+    # Copia el archivo de entrada a la carpeta 
+    shutil.copy('parametros.dat',path_carpeta)
+    #shutil.copy('ising',path_carpeta) 
+    # Se mete en la carpeta
+    os.chdir(path_carpeta)       
+    # Cambia el archivo de entrada adentro de la carpeta
+    isf.escribe_entrada('T',Tnombre)
+    # Corre el programa para ver la convergencia
+    isf.escribe_entrada('N',N_term) 
+
+    ##########################################################################
+    # DISTINTAS CORRIDAS A LA MISMA T PARA OBTENER ERROR ESTADISTICO
+    ##########################################################################
+
+    # Determino si estoy en la zona crítica
+    # para aumentar la cantidad de corridas
+    if T >= T_detail_min and T <= T_detail_max:
+        Nrun = 2 * N_run_base
+        print "Zona crítica: ", Nrun
+    else:
+        Nrun = N_run_base
+        print "Zona No crítica: ", Nrun
+
+    for i in range(0,Nrun):
+    # Nombre de las carpetas con las corridas
+        carpeta_runs = 'RUN' + '%02i'%i
+        # Camino de las carpetas con las corridas
+        path_runs = os.path.join(path_carpeta,carpeta_runs)
+        # Crea las carpetas con las corridas
+        os.makedirs(path_runs)
+        # Copia archivos a cada carpeta
+        shutil.copy('parametros.dat',path_runs)
+        os.chdir(path_runs)
+        # Escribe la semmilla en la carpeta
+        isf.escribe_semilla()
+        
+        #########################################################
+        ######### Para utilizar el estado de temperatura anterior
+        isf.copia_estado_temp_anterior(path_runs,T_anterior,Tnombre)
+        #########################################################
+        
+        #######################################################################
+        # EJECUTA EL PROGRAMA ISING - TERMALIZACIÓN
+        #######################################################################
+        # Esto funciona para python >= 2.7    
+        # salida = subprocess.check_output(curr_dir+'/ising')
+        # Alternativa para python 2.6        
+        proc = subprocess.Popen([curr_dir+'/ising'],stdout=subprocess.PIPE)
+        salida = proc.communicate()[0]
+        # Guardo la salida para ver que hizo
+        with open('log1.txt','w') as arch: arch.write(salida)
+        # Guardo las salidas por si hacen falta
+        if os.path.isfile('energia.dat'):
+            os.rename('energia.dat','energia_terma.dat')
+        elif os.path.isfile('magneti.dat'):
+            os.rename('magneti.dat','magneti_terma.dat') 
+        
+        #########################################################
+        ######### Para utilizar el estado de temperatura anterior
+        os.rename('estado.dat','ultimo_estado.dat')
+        #########################################################
+        
+        #######################################################################
+        # EJECUTA EL PROGRAMA ISING - MEDICIÓN
+        #######################################################################     
+        # Corre por segunda vez tomando el estado anterior. Aumento el N
+        isf.escribe_entrada('N',N_medi)
+        print('Corriendo {0} a la temperatura {1}'.format(carpeta_runs,T))
+        # Esto funciona para python >= 2.7            
+        # salida = subprocess.check_output(curr_dir+'/ising')
+        # Alternativa para python 2.6        
+        proc = subprocess.Popen([curr_dir+'/ising'],stdout=subprocess.PIPE)
+        salida = proc.communicate()[0]
+        # Guardo la salida para ver ue hizo
+        with open('log2.txt','w') as arch: arch.write(salida)
+        # Copia los valores medios
+        isf.copia_val_medios_runs(i,path_carpeta)
+        # Sale de la carpeta de corridas        
+        os.chdir(path_carpeta)
+    # Hace estadística de todas las corridas y lo copia en un archivo
+    isf.copia_val_medios(T,Nrun,curr_dir)
+    # Sale de la carpeta
+    os.chdir(curr_dir)
+    T_anterior = Tnombre
+    
+##############################################################################
 
---
+++ FORTRAN/dinmod/scripts/dinmol_funcs.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import random
+import numpy as np
+
+###############################################################################
+# ESCRIBE EL ARCHIVO DE ENTRADA DE ISING
+###############################################################################
+def escribe_entrada(nombre,valor):
+    with open('parametros.dat','r') as f1:
+        fila = f1.readline()        
+        col  = fila.split()
+        if nombre== 'Temp':
+            col[0] = valor
+        elif nombre == 'N_part':
+            col[1] = valor
+        elif nombre == 'L':
+            col[2] = valor
+        elif nombre == 'dt':
+            col[3] = valor
+        elif nombre == 'N_pasos':
+            col[4] = valor
+        elif nombre == 'N_medi':
+            col[8] = valor
+        else:
+            print('No se reconoce el valor a escribir')
+            exit
+    new = ' '.join(col)
+    with open('parametros.dat','w') as fileout:
+        fileout.write(new)
+###############################################################################
+
+###############################################################################
+# ESCRIBE UN ARCHIVO CON LA SEMILLA PARA ZIGGURAT
+###############################################################################           
+def escribe_semilla():
+    random.seed()
+    # Obtengo un número aleatorio entero de 4 bytes
+    entero_aleatorio = random.randint(-2147483648,2147483647)
+    with open('seed.dat','w') as fileout:
+        fileout.write(str(entero_aleatorio))
+###############################################################################
+
+###############################################################################
+# HACE ESTADISTICA CON LOS RESULTADOS OBTENIDOS A UNA DADA TEMPERATURA
+# ESCRIBE EL ARCHIVO 'tablas_temperatura.dat' EN LA CARPETA T_tmpfolder
+###############################################################################        
+def copia_val_medios(T,N,dire):
+    E=[];M=[];c=[];s=[];a=[]
+    # Copio los archivos de la carpeta local y los escribo en un archivo común
+    with open('runs_estadistica.dat','r') as valruns:
+        for line in valruns:
+            col = [float(x) for x in line.split()]
+            E.append(col[2])
+            c.append(col[3])            
+            M.append(abs(col[4]))
+            s.append(col[5])
+            a.append(col[6])
+    arch_comun = os.path.join(dire,'tablas_temperatura.dat')
+    fa = 1/np.sqrt(N)
+    print('Se promediaron {0} de {1} valores para T = {2}'.format(len(E),N,T))
+    valor = [float(T), np.mean(E), np.std(E)*fa,
+                       np.mean(M), np.std(M)*fa,
+                       np.mean(c), np.std(c)*fa,
+                       np.mean(s), np.std(s)*fa,
+                       np.mean(a), np.std(a)*fa]
+    # Abro el archivo donde se volcarán las resultados de cada temperatura   
+    with open(arch_comun,'a') as comun:  
+        valor_str = '  '.join( format(j,'.5E') for j in valor)         
+        comun.write( valor_str +'\n')   
+###############################################################################
+
+###############################################################################
+# ESCRIBE EL ARCHIVO CON LOS RESULTADOS DE CADA CORRIDA A UNA DADA TEMPERATURA
+# LEE 'val_medios.dat' EN CADA CARPETA RUNXX
+# ESCRIBE EL ARCHIVO 'runs_estadistica.dat' EN LA CARPETA T_tmpfolder
+# ---- Es usado sólo por corridas.py 
+# ---- corridas_paralelo.py usa dos funciones separadas (por la comunicación)
+###############################################################################            
+def copia_val_medios_runs(i,path):
+    # Copio los archivos de la carpeta local y los escribo en un archivo común
+    with open('val_medios.dat','r') as vmed:
+        valor=vmed.readline()
+    # Lee el archivo con la cantidad de aceptaciones
+    with open('aceptaciones.dat','r') as archacept:
+        acep=archacept.readline()
+        colu = [float(x) for x in acep.split()]
+        # Guardará el porcentaje de aceptaciones
+        por_acep = 100.0*colu[1]/colu[2]
+    arch_comun = os.path.join(path,'runs_estadistica.dat')
+    with open(arch_comun,'a') as comun:
+        comun.write('%02i'%i + ' ' + valor.rstrip() + ' '+ str(por_acep) +'\n') 
+###############################################################################
+
+        
+###############################################################################
+# LEE 'val_medios.dat' Y 'aceptaciones.dat' EN CADA CARPETA RUNXX 
+# LOS ORDENA EN UN STRING Y LOS PASA COMO SALIDA
+###############################################################################            
+
+def lee_datos_runs(i):
+    # Copio los archivos de la carpeta local y los escribo en un archivo común
+    with open('val_medios.dat','r') as vmed:
+        valor=vmed.readline()
+    # Lee el archivo con la cantidad de aceptaciones
+    with open('aceptaciones.dat','r') as archacept:
+        acep=archacept.readline()
+        colu = [float(x) for x in acep.split()]
+        # Guardará el porcentaje de aceptaciones
+        por_acep = 100.0*colu[1]/colu[2]
+    # Ordena los valores leidos en un string y los pasa como salida
+    lin_dat = '%02i'%i + ' ' + valor.rstrip() + ' '+ str(por_acep) +'\n'
+    return lin_dat
+###############################################################################
+
+###############################################################################
+# ESCRIBE EL ARCHIVO 'runs_estadistica.dat' EN LA CARPETA T_tmpfolder
+###############################################################################            
+
+def escribe_datos_runs(datos_str,path):
+    arch_comun = os.path.join(path,'runs_estadistica.dat')
+    with open(arch_comun,'a') as comun:
+        comun.write(datos_str)
+###############################################################################
+
+###############################################################################
+# BUSCA EL ARCHIVO CON EL ESTADO FINAL A LA TEMPERATURA ANTERIOR Y LO COPIA
+# EN LA CARPETA DE LA TEMPERATURA ACTUAL
+###############################################################################      
+def copia_estado_temp_anterior(path_act,T_ant,T_act):
+    # Copia y reescribe el último estado calculado a una temperatura anterior
+    if T_ant != []:
+        # Reemplazo sólo la parte de la temperatura en toda la ruta
+        path_ant = path_act.replace(T_act,T_ant)
+        # Ruta completa del archivo a copiar
+        # arch_ant = os.path.join(path_ant,'estado.dat')
+        # Para todas las corridas utiliza el ultimo estado de RUN00
+        arch_ant = os.path.join(os.path.dirname(path_ant),'RUN00','estado.dat')        
+        # Copia el archivo
+        shutil.copy(arch_ant,path_act)
+        # reescribe el archivo
+        os.rename('estado.dat','ultimo_estado.dat')
+###############################################################################
