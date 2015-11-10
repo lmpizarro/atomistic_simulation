14faceb6d9ce4632f7f22aca9be0be6badd44c04
Pablo Bellino <pbellino@gmail.com>
2015-11-10 12:17:49 -0300

    Se arma script para graficar los valores finales del archivo tabla_temperatura.dat

--- FORTRAN/dinmod/scripts/graficos_tablas_t.py
+++ FORTRAN/dinmod/scripts/graficos_tablas_t.py
@@ -14,79 +14,78 @@ if len(sys.argv) < 2:
 else:
     archivo = sys.argv[1:]
     
-Nsp = []
-Nsp.append(20*20)
-
 forma = []
-forma.append('k.')
+forma.append('b.')
 
 leg = []
-leg.append('20 x 20')
+leg.append(ur'$\rho^* = -1.3$')
+
+xLim = [0.65, 1.45]
 
 j = 0
 for nombre in archivo:
 
-    data = np.loadtxt(nombre)
+    data = np.loadtxt(nombre,skiprows=1)
     
-    T = data[:,0]
-    E_avg = data[:,1]
-    E_std = data[:,2]
-    M_avg = np.abs(data[:,3])
-    M_std = data[:,4]
-    c_avg = data[:,5]
-    c_std = data[:,6]
-    s_avg = data[:,7]
-    s_std = data[:,8]
-    a_avg = data[:,9]
-    a_std = data[:,10]
+    T     = data[:,0]
+    rho   = data[:,1]
+    p_avg = data[:,2]
+    p_std = data[:,3]
+    c_avg = data[:,4]
+    c_std = data[:,5]
+    T_avg = data[:,6]
+    T_std = data[:,7]
+    U_avg = data[:,8]
+    U_std = data[:,9]
     
-    plt.figure(1,figsize=(8,8))
+    plt.figure(1,figsize=(8,6))
     
-    plt.subplot(2, 1, 1)
-    plt.errorbar(T, E_avg/Nsp[j], yerr=E_std/Nsp[j], fmt=forma[j],label=leg[j])
+    plt.errorbar(T_avg, p_avg, xerr=T_std, yerr=p_std, fmt=forma[j],label=leg[j])
     #plt.title(r'Valores medios')
-    plt.xlabel('$kT/J$')
-    plt.ylabel('$<E/J>/N$')
-    plt.ylim([-2.1 , -0.2])
+    plt.xlabel('$T^*$')
+    plt.ylabel('$p^*$')
+    plt.xlim(xLim)
+    #plt.ylim([-2.1 , -0.2])
     plt.legend(loc='upper left',numpoints=1)
     plt.grid(True)
    
+    plt.figure(2,figsize=(8,6))
     
-    plt.subplot(2, 1, 2)
-    plt.errorbar(T, M_avg/Nsp[j],yerr=M_std/Nsp[j], fmt= forma[j],label=leg[j])
-    plt.xlabel('$kT/J$')
-    plt.ylabel('$<M/\mu>/N$')
-    plt.ylim([-0.1 , 1.1])
-    plt.legend(loc='upper right',numpoints=1)    
+    plt.errorbar(T_avg, c_avg, xerr=T_std,yerr=c_std, fmt= forma[j],label=leg[j])
+    plt.xlabel('$T^*$')
+    plt.ylabel('$\sigma(p^*)$')
+    plt.xlim(xLim)
+    #plt.ylim([-0.1 , 1.1])
+    plt.legend(loc='upper center',numpoints=1)    
     plt.grid(True)
     
-    plt.figure(2,figsize=(8,8))
+    plt.figure(3,figsize=(8,6))
     
-    plt.subplot(2, 1, 1)
-    plt.errorbar(T, c_avg/Nsp[j], yerr=c_std/Nsp[j], fmt=forma[j],label=leg[j])
-    #plt.title('Fluctuaciones')
-    plt.xlabel('$kT/J$')
-    plt.ylabel('$<C>/N$')
-    plt.ylim([-0.1 , 2.3])
-    plt.legend(loc='upper right',numpoints=1)  
+    plt.errorbar(T_avg, U_avg, xerr=T_std,yerr=U_std, fmt= forma[j],label=leg[j])
+    plt.xlabel('$T^*$')
+    plt.ylabel('$U^*$')
+    plt.xlim(xLim)
+    #plt.ylim([-0.1 , 1.1])
+    plt.legend(loc='upper left',numpoints=1)    
     plt.grid(True)
-    
-    plt.subplot(2, 1, 2)
-    plt.errorbar(T, s_avg/Nsp[j], yerr=s_std/Nsp[j], fmt=forma[j],label=leg[j])
-    plt.xlabel('$kT/J$')
-    plt.ylabel('$<\chi>/N$')
-    plt.ylim([-4 , 100])
-    plt.legend(loc='upper right',numpoints=1)  
-    plt.grid(True)
-    
-    plt.figure(3)
-    
-    plt.errorbar(T,a_avg,yerr=a_std,fmt=forma[j],label=leg[j])
-    plt.xlabel('$kT/J$')
-    plt.ylabel('$Aceptaciones [\%]$')
-    plt.ylim([-5 , 75])
-    plt.legend(loc='upper left',numpoints=1)
-    plt.grid(True)
+#    plt.figure(2,figsize=(8,8))
+#    
+#    plt.subplot(2, 1, 1)
+#    plt.errorbar(T, c_avg/Nsp[j], yerr=c_std/Nsp[j], fmt=forma[j],label=leg[j])
+#    #plt.title('Fluctuaciones')
+#    plt.xlabel('$kT/J$')
+#    plt.ylabel('$<C>/N$')
+#    plt.ylim([-0.1 , 2.3])
+#    plt.legend(loc='upper right',numpoints=1)  
+#    plt.grid(True)
+#    
+#    plt.subplot(2, 1, 2)
+#    plt.errorbar(T, s_avg/Nsp[j], yerr=s_std/Nsp[j], fmt=forma[j],label=leg[j])
+#    plt.xlabel('$kT/J$')
+#    plt.ylabel('$<\chi>/N$')
+#    plt.ylim([-4 , 100])
+#    plt.legend(loc='upper right',numpoints=1)  
+#    plt.grid(True)
     
     j += 1
 
