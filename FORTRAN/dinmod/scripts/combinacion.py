2d6ae028d6938927c1032f620a94674d62cc81c0
Luis María Pizarro <lmpizarro@gmail.com>
2015-11-18 15:53:01 -0300

    combinacion.py calculo de parametros de interaccion para mezclas cont.

--- FORTRAN/dinmod/scripts/combinacion.py
+++ FORTRAN/dinmod/scripts/combinacion.py
@@ -1,12 +1,17 @@
 
 # coding: utf-8
 
-# In[17]:
+# In[46]:
 
 import numpy as np
 
-# Dimensión del problema
-Rho = .1
+
+#
+# Calcula la cantidad de partículas de un sistema de Nespecies
+# y la cantidad de partículas por especies dados los porcentajes
+# atómicos de Nespecies - 1
+#
+Rho = .9
 L = 5 # veces sigma_a
 percent_a = 0.3
 percent_b = 0.5
@@ -18,6 +23,16 @@ Nc = Npart - Na - Nb
 
 Np = np.asarray([Na,Nb, Nc])
 
+print "cantidad de partículas total %d" % (Npart)
+
+for i in range(np.size(Np)):
+    print "cantidad de partículas del elemento %d: %d" % (i, Np[i])
+
+    
+#
+# Calcula los parámetros de interacción de un sistema
+# de Nespecies, dados los vectores epsilon y sigma
+#    
 #epsilon en eV
 epsilon = np.asarray([1.83592*10E-2, 1.03413*10E-2, 1.2*10E-2])
 #sigma en angstrom
@@ -58,10 +73,6 @@ Epsilon = calc_matriz_epsilon(epsilon) / epsilon[0]
 Sigma =  calc_matriz_sigma(sigma) / sigma[0]
 
 
-print Epsilon
-print Sigma
-
-
 #inicializa posiciones
 P = np.random.rand((Npart)*3).reshape(Npart, 3) * L
 V = np.random.rand((Npart)*3).reshape(Npart, 3) - 0.5
@@ -70,13 +81,15 @@ V = np.random.rand((Npart)*3).reshape(Npart, 3) - 0.5
 # vcm = (Ma * Sum (vai) + Mb * Sum (vbi) ) / (Na * Ma + Nb * Mb)
 
 
-#Vcm = (Ma * np.sum(V[:Na], 0) + Mb * np.sum(V[Na:], 0)) /  (Na * Ma + Nb * Mb)
+Vcm = np.zeros(3)
+inic = 0; fin = 0
+for i in range(np.size(Np)):
+    fin = inic + Np[i]
+    Vcm = Vcm + masa[i] * np.sum(V[inic:fin],0)
+    inic = Np[i]
 
-
-#print (V)
-#print (np.sum(V[:Na], 0))
-#print (np.sum(V[Na:], 0))
-#print (Vcm)
+Vcm = Vcm / np.sum(Np * masa)
+print "Velocidad del centro de masa: %.3f %.3f %.3f" % (Vcm [0], Vcm[1], Vcm[2])
 
 
 # In[58]:
