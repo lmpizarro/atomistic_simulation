e0ece0d9990f683871e414f5b3e538b528ddccdb
Luis María Pizarro <lmpizarro@gmail.com>
2015-11-19 09:49:22 -0300

    combinacion.f90 cont.

--- FORTRAN/DMMulti/combinacion.f90
+++ FORTRAN/DMMulti/combinacion.f90
@@ -9,14 +9,36 @@ module combinacion
 
   implicit none
 
+  integer :: i, j
+
 contains
 
   subroutine comb_epsilon()
-    ALLOCATE (gCombEpsilon(1:gNespecies,1:gNespecies))
+    allocate (gCombEpsilon(1:gNespecies,1:gNespecies))
+    do i=1, gNespecies
+      gCombEpsilon(i,i) = gLj_param(i,1)
+    enddo
+
+    do i =1, gNespecies
+      do j=i+1, gNespecies
+         gCombEpsilon(i,j) = gLj_param(i,1) *  gLj_param(j,1)
+         gCombEpsilon(j,i) = gCombEpsilon(i,j) 
+      enddo
+    enddo
   endsubroutine comb_epsilon
 
   subroutine comb_sigma()
-    ALLOCATE (gCombSigma(1:gNespecies,1:gNespecies))
+    allocate (gCombSigma(1:gNespecies,1:gNespecies))
+    do i=1, gNespecies
+      gCombSigma(i,i) = gLj_param(i,2)
+    enddo
+    do i =1, gNespecies
+      do j=i+1, gNespecies
+         gCombSigma(i,j) =  (gLj_param(i,2) + gLj_param(j,2)) / 2
+         gCombSigma(j,i) = gCombSigma(i,j)
+      enddo
+    enddo
+
   endsubroutine comb_sigma
 
 endmodule combinacion
