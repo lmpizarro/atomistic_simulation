f059a170244cfd7869a9d4b11d6d957e8f271bb9
Pablo Bellino <pbellino@gmail.com>
2015-12-01 16:57:23 -0300

    Se pone la subrutina similar a la de dinmod para integrar con velocity-verlet

--- FORTRAN/DMMulti/libs/integra.f90
+++ FORTRAN/DMMulti/libs/integra.f90
@@ -1,8 +1,10 @@
 module integra
 
+#include "control.h"
+
   use types,             only: dp
   use globales
-  use mediciones
+  use mediciones,        only: calcula_pres, calcula_temp, calcula_kin, calcula_fuerza 
   use io_parametros,     only: escribe_trayectoria, escribe_en_columnas
 
   implicit none
@@ -17,7 +19,120 @@ contains
   ! integración de las ecuaciones de movimiento - velocity verlet
   !===============================================================================
   ! integra las ecuaciones dinámicas con el algoritmo de velocity-verlet
+
   subroutine integracion()
+
+    real(dp)                              :: pres    ! presión instantánea
+    real(dp)                              :: temp    ! presión instantánea
+    integer                               :: Kmed    ! Cantidad de puntos medidos
+    real(dp), dimension(:,:), allocatable :: Eng_t   ! energía en función del tiempo
+    real(dp), dimension(:), allocatable   :: Pres_t  ! presión en función del tiempo
+    real(dp), dimension(:), allocatable   :: Temp_t  ! temperatura en función del tiempo
+
+    character(50), parameter :: nombre='trayectoria.vtf' ! Nombre archivo para guardar trayectorias
+    integer :: i, j
+
+    ! Se define la cantidad de puntos que se van a medir
+    Kmed = int(gNtime/abs(gNmed)) + 1              ! Se agrega +1 para poner el inicial
+    allocate( Eng_t(1:3,1:Kmed), Pres_t(1:Kmed), Temp_t(1:Kmed))
+
+    ! para acumular velcidades en función del tiempo para el calculo 
+    ! de correlaciones
+    allocate (gCorrVfac_1(1:3, 1:Kmed))
+    allocate (gCorrVfac_2(1:3, 1:Kmed))
+    allocate (gCorrVver_1(1:3, 1:Kmed))
+    allocate (gCorrVver_2(1:3, 1:Kmed))
+
+#ifdef GRABA_TRAYECTORIA
+    call escribe_trayectoria(gR,nombre,.TRUE.)
+#endif
+
+    ! El primer punto son los valores iniciales
+    Eng_t(:,1) = (/ gPot, gKin, gPot + gKin/) 
+    call calcula_pres(Pres)
+    Pres_t(1) = Pres
+    call calcula_temp(Temp)
+    Temp_t(1) = Temp
+
+    ! -------------------------------------------------------------------------------------------
+    ! COMIENZA EL LOOP PRINCIPAL DE INTEGRACION
+    ! ------------------------------------------------------------------------------------------
+    j = 2                                              ! Contador para el loop de mediciones
+    do i = 1, gNtime 
+      gR = gR + gDt * gV + 0.5_dp *  gF * gDt ** 2 / gMasa(gIndice_elemento(j))   ! gR(t+dt)
+      ! Aplica condiciones peródicas de contorno
+      call cpc_vec()
+      gV = gV + 0.5_dp * gF * gDt / gMasa(gIndice_elemento(j))     ! gV(t+0.5dt) 
+      call calcula_fuerza()                            ! Calcula fuerzas y potencial
+      gV = gV + 0.5_dp * gF * gDt / gMasa(gIndice_elemento(j))     ! gV(t+dt)
+      !
+      ! Agregar en un array velocidades (t) para calcular correlaciones
+      ! El calculo va sobre una posición equivalente en la red cristalina
+      ! En la red fcc la posición 1 mod 4 en gR son equivalentes
+      ! Otra posición equivalente es 2 mod 4, 3 mod 4 y 4 mod 4  
+      !
+
+      ! Se realizan las mediciones
+      if (mod(i,gNmed) == 0) then
+        ! Energia cinetica
+        call calcula_kin()
+        ! Temperatura
+        call calcula_temp(Temp)
+        ! Presión
+        call calcula_pres(Pres)
+        ! Guarda magnitudes
+        Eng_t(:,j) = (/gPot, gKin, gPot + gKin/) 
+        Pres_t(j)  = Pres
+        Temp_t(j)  = Temp
+       
+#ifdef GRABA_TRAYECTORIA
+    call escribe_trayectoria(gR,nombre,.FALSE.)
+#endif
+        j = j + 1                                      ! Actualiza contador mediciones
+      end if
+    end do
+! -------------------------------------------------------------------------------------------
+! FIN DEL LOOP PRINCIPAL DE INTEGRACION
+! -------------------------------------------------------------------------------------------
+   
+    ! Escritura de las magnitudes en funci..n del tiempo
+    if (gNmed > 0) then
+      ! Guarda las energ..as por part..cula  en un archivo [Potencial, Cin..tica, Total]
+      call escribe_en_columnas(Eng_t/gNpart,'energias.dat',gNmed*gDt)
+
+      ! Guarda la presion en un archivo
+      call escribe_en_columnas(Pres_t,'presion.dat',gNmed*gDt)
+      
+      ! Guarda la temperatura en un archivo
+      call escribe_en_columnas(Temp_t,'temperatura.dat',gNmed*gDt)
+    end if
+
+    ! Se imprime en pantalla los resultados finales
+    write(*,*) '* Energias por partícula al final de la integración'
+    call print_info(Pres,Temp)
+
+    deallocate( Eng_t, Pres_t, Temp_t)
+
+  contains
+
+    subroutine print_info(presion,temperatura)
+    ! Subrutina para imprimir en pantalla resultados de interes
+ 
+      real(dp), intent(in)        :: presion     ! Presi..n instant..nea
+      real(dp), intent(in)        :: temperatura ! Temperatura instant..nea
+
+      write(*,100) gPot/gNpart, gKin/gNpart, (gPot+gKin)/gNpart
+      100 format(1X,'Potencial = ', E14.7, 5X, 'Cinética = ', E14.7, 5X, 'Total = ', E14.7) 
+      write(*,*)
+      write(*,200)  presion, temperatura
+      200 format(1X,'Presión = ' ,E14.7,5X,'Temperatura = ',E14.7)
+
+    end subroutine print_info
+
+  end subroutine integracion
+
+  subroutine integracion_original()
+
     real(dp) :: pres    ! presión instantánea
     real(dp) :: temp    ! presión instantánea
     integer :: Kmed    ! Cantidad de puntos medidos
@@ -101,7 +216,7 @@ contains
 ! -------------------------------------------------------------------------------------------
     print *, "fin integracion"
     deallocate( Eng_t, Pres_t, Temp_t)
-  endsubroutine integracion
+  endsubroutine integracion_original
 
   !===============================================================================
   ! INTEGRACIÓN DE LAS ECUACIONES DE MOVIMIENTO - MINIMIZACIÓN ENERGÍA
@@ -113,7 +228,7 @@ contains
     real(dp), dimension(gNtime+1) :: Eng_t   ! Energía en función del tiempo
     integer :: i, j
     ! Nombre del archivo para guardar la trayectoria
-    character(50), parameter   :: nombre='trayectoria_minimiza.vtf'
+    character(50), parameter   :: nombre='trayectoria_pot_minimiza.vtf'
      
     ! Escribe encabezado y primer punto de la trayectoria
     call escribe_trayectoria(gR,nombre,.TRUE.) 
