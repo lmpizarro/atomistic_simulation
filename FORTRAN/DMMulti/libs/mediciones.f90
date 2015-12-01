337bd6bba9892a28b9f1916510e73516e4157a78
Pablo Bellino <pbellino@gmail.com>
2015-12-01 16:57:54 -0300

    Se arregla subrutina para calcular la energía cinética

--- FORTRAN/DMMulti/libs/mediciones.f90
+++ FORTRAN/DMMulti/libs/mediciones.f90
@@ -316,28 +316,14 @@ contains
   ! Calcula la anergia cinetica total del sistema
   subroutine calcula_kin()
     
-    real(dp), allocatable   :: v2(:)    ! Vector con la velocidad cuadratica
-    real(dp) :: masa
-    integer :: inic, fin, i
-
-
-    allocate(v2(1:gNpart))
+    real(dp), dimension(gNpart)   :: mv2    ! Vector con la masa* velocidad cuadratica
+    integer ::  i
 
     do i = 1, gNpart
-      v2(i) =  dot_product(gV(:,i),gV(:,i))  
+      mv2(i) =  gMasa(gIndice_elemento(i))*dot_product(gV(:,i),gV(:,i))  
     end do
 
-    gKin = 0.0_dp
-    masa = 0.0_dp 
-    inic = 1
-    do i=1, gNespecies
-      fin = gNp(i) + inic
-      masa = gLj_param(i, 3)
-      gKin = gKin + masa * sum(v2(inic:fin))
-      inic = fin + 1
-    enddo
-
-    gKin = 0.5_dp * gKin
+    gKin = 0.5_dp * sum(mv2)
 
   endsubroutine calcula_kin
 
