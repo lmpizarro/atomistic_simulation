c809672852c109720a30c909b8558c4a7036dd7e
Pablo <pbellino@gmail.com>
2015-12-05 18:24:49 -0300

    Se agrega archivo con utilidades. Se incorpora medición de tiempo

--- FORTRAN/DMMulti/src/main.f90
+++ FORTRAN/DMMulti/src/main.f90
@@ -2,13 +2,23 @@ program main
 
   use inic_fin,        only: inicializacion, finalizacion 
   use integra,         only: integracion
+  use types,           only: dp
+  use utils,           only: wtime
 
   implicit none
+
+  real(dp)   :: wt ! Variable auxiliar para calcular tiempos de ejecución
+
+  wt = wtime()
 
   call inicializacion()
 
   call integracion()
 
   call finalizacion()
+
+  write(*,*) '********************************'
+  write(*,*) 'Tiempo transcurrido: ', wtime()-wt
+  write(*,*) '********************************'
 
 endprogram main
