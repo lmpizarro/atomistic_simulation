aee20917e629117a9c469286f099d96562fe187f
Luis María Pizarro <lmpizarro@gmail.com>
2015-12-11 13:25:31 -0300

    lectura en fortran de parametrizacion creada en python cont.

--- FORTRAN/carlosMontes/libs/read_md.f90
+++ FORTRAN/carlosMontes/libs/read_md.f90
@@ -2,10 +2,14 @@ module param_md
   use types, only: dp
   implicit none
 
+  real(dp),  dimension(:,:), allocatable  :: gR    ! Posicion de las partículas
+
   real(dp),  dimension(:,:), allocatable  :: gCombSigma    ! Matriz con sigma
   real(dp),  dimension(:,:), allocatable  :: gCombEpsilon  ! Matriz con epsilon
   real(dp), allocatable :: gRc2(:,:)  ! Cuadrado del radio de corte
   real(dp), allocatable :: gPot_Cut(:,:)  ! Potencial L-J en el radio de corte
+  real(dp),  dimension(:), allocatable    :: gMasa         ! Vector de masas
+  integer, allocatable :: gIndice_elemento(:)      ! Indice de partícula
 
 
   type Parametros
@@ -15,6 +19,7 @@ module param_md
     integer  :: gNmed          ! Cantidad de pasos entre mediciones
     real(dp) :: gGamma         ! Parámetro para el termostato de Langevin  
     integer  :: gNespecies      ! Cantidad de especies en el sistema
+    integer  :: gNpart         ! Cantidad de partículas del sistema
   contains
     procedure :: leer => read_parameters
   end type Parametros
@@ -28,14 +33,18 @@ contains
     inquire(file='./config.par',exist=es)
     if(es) then
       open(unit=10,file='./config.par',status='old')
+      !lectura de los parámetros de corrida
       read(10,*) this%gTemperatura, this%gDt, this%gNtime, this%gNmed,&
          this%gGamma
+      !lee cantidad de especies
       read(10,*) this%gNespecies
-
+      
+      ! alloca los arrays necesarios para leer configuraciones
       allocate(gCombSigma(1:this%gNespecies, 1:this%gNespecies))
       allocate(gCombEpsilon(1:this%gNespecies, 1:this%gNespecies))
       allocate(gRc2(1:this%gNespecies, 1:this%gNespecies))
       allocate(gPot_Cut(1:this%gNespecies, 1:this%gNespecies))
+      allocate(gMasa(1:this%gNespecies))
 
       do i = 1, this%gNespecies
         read (10,*) (gCombSigma (i,j), j = 1,this%gNespecies)
@@ -53,6 +62,19 @@ contains
         read (10,*) (gPot_Cut (i,j), j = 1,this%gNespecies)
       enddo
 
+      do i = 1, this%gNespecies
+        read (10,*) gMasa (i)
+      enddo
+
+      read(10,*) this%gNpart
+
+      ! alloca para leer posiciones de partículas
+      allocate(gR(1:3, 1:this%gNpart))
+      allocate(gIndice_elemento(1:this%gNpart))
+
+      do i = 1, this%gNpart
+        read (10,*) (gR (i,j), j = 1,3), gIndice_elemento(i)
+      enddo
 
       close(10)
     else
@@ -75,14 +97,26 @@ contains
       write (*, 700) (gRc2 (i,j), j = 1,this%gNespecies)
     enddo
 
-    do i = 1, this%gNespecies
-      write (*, 700) (gPot_Cut (i,j), j = 1,this%gNespecies)
+    do i = 1, this%gnespecies
+      write (*, 700) (gpot_cut (i,j), j = 1,this%gnespecies)
+    enddo
+
+    do i = 1, this%gnespecies
+      write (*, 300) (gMasa (i))
+    enddo
+
+    write(*,200) this%gNpart
+
+    do i = 1, this%gNpart
+      write (*,800) (gR (i,j), j = 1,3), gIndice_elemento(i)
     enddo
 
 
     100 format (F14.8,F14.8,I10,I10,F14.8)
     200 format (I10)
+    300 format (F14.8)
     700 format (100g15.5)
+    800 format (3g15.5, I10)
   endsubroutine   
 
 endmodule param_md
