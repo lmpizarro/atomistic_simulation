030011dcffa197a30d6e9aef3a0f3d3ea4eeff07
lmpizarro <lmpbici@gmail.com>
2015-12-11 01:24:49 -0300

    cubic corregido para generar en python

--- FORTRAN/carlosMontes/libs/cubic.py
+++ FORTRAN/carlosMontes/libs/cubic.py
@@ -30,14 +30,17 @@ class Cubic(object):
        self.NombreElementos = ["C"] * self.Natoms
 
        if (tipo == "fcc"):
-           self.Positions = cubic.posiciones_fcc(self.Positions, self.Nperx, 
-                   self.Npery, self.Nperz, 3, self.Natoms) 
+          # self.Positions = cubic.posiciones_fcc(self.Positions, self.Nperx, 
+          #         self.Npery, self.Nperz, 3, self.Natoms) 
+	  pass
        elif (tipo =="simple"):
-           self.Positions = cubic.posiciones_simple(self.Positions, self.Nperx, 
-                   self.Npery, self.Nperz, 3, self.Natoms) 
+          # self.Positions = cubic.posiciones_simple(self.Positions, self.Nperx, 
+          #         self.Npery, self.Nperz, 3, self.Natoms) 
+	  pass
        elif (tipo =="bcc"):
-           self.Positions = cubic.posiciones_bcc(self.Positions, self.Nperx, 
-                   self.Npery, self.Nperz, 3, self.Natoms) 
+          # self.Positions = cubic.posiciones_bcc(self.Positions, self.Nperx, 
+          #         self.Npery, self.Nperz, 3, self.Natoms) 
+	  pass
 
     def list_positions (self):
         print self.IndiceElementos
@@ -45,6 +48,26 @@ class Cubic(object):
         for i in range(np.size(self.Positions[0])):
             print  self.IndiceElementos[i], self.Positions[0][i], \
                   self.Positions[1][i], self.Positions[2][i]
+
+    def generate_simple(self):
+	p = np.zeros(3*self.Nperx* self.Npery* self.Nperz).reshape(3, self.Nperx* self.Npery* self.Nperz)
+        # 0 0     0 1     0 2    
+        # 1 0     1 1     1 2
+	# 2 0     2 1     2 2
+        
+	igr = 0
+        for i in range(self.Nperx):
+          for j in range(self.Npery):
+            for k in range(self.Nperz):
+                  p[0][igr] = i
+                  p[1][igr] = j
+                  p[2][igr] = k
+		  igr += 1
+    	print p	  + np.array([0.5,0.5, 0.0]).reshape(3,1) 
+
+
+
+	#print p.shape
 
     # representacion xyz de la celda básica
     def write_xyz(self, name):
@@ -85,7 +108,7 @@ class Cubic(object):
         pass
 
 def main ():
-    fcc = Cubic(3, tipo="fcc")
+    fcc = Cubic(2, nx = 3,ny = 3,nz = 3,   tipo="fcc")
     print fcc.Natoms
     print fcc.Nperx, fcc.Npery, fcc.Nperz
     print fcc.Positions
@@ -94,6 +117,7 @@ def main ():
     fcc.write_vtf("test.vtf")
 
     fcc.list_positions()
+    fcc.generate_simple()
 
 
 if __name__ == "__main__":
