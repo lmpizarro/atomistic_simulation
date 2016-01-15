940a094eb3d84824f5c8318998ba2d1474ad206f
Luis María Pizarro <lmpizarro@gmail.com>
2016-01-15 19:24:54 -0300

    agregado del monitor de cluster en django con mongo

--- ASE/lammps/UZr/equilibraPy/nptPy/main_npt.py
+++ ASE/lammps/UZr/equilibraPy/nptPy/main_npt.py
@@ -13,38 +13,7 @@ import gen_runs as gr
 import parametros_t as pars
 
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     gr.run_npt(root_path, pars.dict_config)
 
 
-=======
-    run_folder = 'relax_calc' 
-    path_run_folder = os.path.join(root_path,run_folder)
-    os.makedirs(path_run_folder)
-
-    # genera el run.qsub para sheldon
-    cant_nodos = 8
-    qs1 = qs.Qsub_sheldon("UZrEquilibration", cant_nodos, "infile_equil")
-    qs1.set_run_path (path_run_folder)
-    qs1.gen_file()
-
-    # graba los potenciales de acuerdo a como los espera el infile
-    pot =  pots.MEAMPOT_UZr("meam", "UZr")
-    pot.gen_files(path_run_folder)
-    pair = pot.get_lammps_pot()
-
-    # Default lattice parameter
-    a0=3.47
-    # cantidad de períodos de la red cristalina
-    np = 8
-    # pasos grabación
-    s_steps = 100 
-    # cantidad de pasos calculo
-    n_steps = 10000
-    min = gl.Equil_NPT (a0, np, "infile", pair, n_steps=n_steps, s_steps=s_steps)
-    min.set_temperature (100)
-    min.set_seed (23456)
-    # genera el infile para lammps
-    min.gen_infile(path_run_folder)
->>>>>>> edc0a70ac55bab03af6488b3d8e05a2d93d39f66
